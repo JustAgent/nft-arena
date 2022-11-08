b67a62c6623f501248e1b0981d7c95041f699c43
JustAgent <baykov.ivan.03@gmail.com>
2022-11-08 22:44:37 +0300

    arena coin

--- contracts/Arena.sol
+++ contracts/Arena.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+contract Arena is ERC721, Ownable{
+
+ArenaCoin arenaCoin;
 uint16 constant MAX_COUNT  = 30000;
 uint16 constant MAX_LEGENDARY = 1000;
-
-contract Arena is ERC721{
-
 
 // MAX STATS:
 // - [ ] HP 40.000 / 100.000
@@ -34,9 +37,12 @@ contract Arena is ERC721{
     uint256 power; // Может логичнее каждый раз ее считать?
   }
 
+
   enum Race { Orcs, Dragons, Elves, Humans}
 
-  constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){}
+  constructor(string memory _name, string memory _symbol, address _arenaCoin) ERC721(_name, _symbol){
+    arenaCoin = ArenaCoin(_arenaCoin);
+  }
 
   function _fight(Fighter memory fighter1, Fighter memory fighter2)  private returns(uint16) {
     // uint24 hp1 = fighter1.hp;
@@ -52,4 +58,17 @@ contract Arena is ERC721{
   function _rewarding(address _winner) private {
 
   }
+}
+
+
+contract ArenaCoin is ERC20, Ownable {
+
+  constructor(string memory _name, string memory _symbol)  ERC20(_name, _symbol) {
+    mint(msg.sender, 1000000000000); // 1.000.000.000.000
+  }
+
+  function mint(address _to, uint256 _amount) public onlyOwner {
+    _mint(_to, _amount);
+  }
+
 }
